4cf86c989acca1a9d986ee0700d0b103cd3c94f8
Mathieu-bt <bouty.mathieu@gmail.com>
2026-01-16 12:42:25 -0500

    Fix hello-univer locale init and facade usage

---
+++ examples/hello-univer/main.js
@@ -0,0 +1,61 @@
+// Import CSS
+import '@univerjs/design/lib/index.css';
+import '@univerjs/ui/lib/index.css';
+import '@univerjs/docs-ui/lib/index.css';
+import '@univerjs/sheets-ui/lib/index.css';
+
+import enUS from '@univerjs/mockdata/locales/en-US';
+
+// Import Univer core
+import { Univer, LocaleType, UniverInstanceType } from '@univerjs/core';
+import { FUniver } from '@univerjs/core/facade';
+
+// Import facade side-effects
+import '@univerjs/sheets/facade';
+import '@univerjs/ui/facade';
+import '@univerjs/docs-ui/facade';
+import '@univerjs/sheets-ui/facade';
+import '@univerjs/engine-formula/facade';
+import '@univerjs/sheets-formula/facade';
+import '@univerjs/sheets-numfmt/facade';
+
+// Import plugins
+import { UniverRenderEnginePlugin } from '@univerjs/engine-render';
+import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula';
+import { UniverUIPlugin } from '@univerjs/ui';
+import { UniverDocsPlugin } from '@univerjs/docs';
+import { UniverDocsUIPlugin } from '@univerjs/docs-ui';
+import { UniverSheetsPlugin } from '@univerjs/sheets';
+import { UniverSheetsUIPlugin } from '@univerjs/sheets-ui';
+import { UniverSheetsFormulaPlugin } from '@univerjs/sheets-formula';
+import { UniverSheetsNumfmtPlugin } from '@univerjs/sheets-numfmt';
+
+// 1. Create Univer instance
+const univer = new Univer({
+    locale: LocaleType.EN_US,
+    locales: {
+        [LocaleType.EN_US]: enUS,
+    },
+});
+
+// 2. Register plugins
+univer.registerPlugin(UniverRenderEnginePlugin);
+univer.registerPlugin(UniverFormulaEnginePlugin);
+univer.registerPlugin(UniverUIPlugin, {
+    container: 'app',
+});
+univer.registerPlugin(UniverDocsPlugin);
+univer.registerPlugin(UniverDocsUIPlugin);
+univer.registerPlugin(UniverSheetsPlugin);
+univer.registerPlugin(UniverSheetsUIPlugin);
+univer.registerPlugin(UniverSheetsFormulaPlugin);
+univer.registerPlugin(UniverSheetsNumfmtPlugin);
+
+// 3. Create a new empty workbook
+univer.createUnit(UniverInstanceType.UNIVER_SHEET, {});
+
+// 4. Expose API for console testing
+window.univerAPI = FUniver.newAPI(univer);
+
+console.log('Univer Hello World initialized!');
+console.log('Try: univerAPI.getActiveWorkbook().getActiveSheet().getRange("A1").setValue("Hello!")');
